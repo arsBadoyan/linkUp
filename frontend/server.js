5dd8044e7bc1ee17464b3a5d9e6909f136f07384
Arsen Badoyan <arsenbadoyan@MacBook-Air-Arsen.local>
2025-05-22 14:15:12 +0400

    b

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,6 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import compression from 'compression';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
 
 const app = express();
 const PORT = process.env.PORT || 5173;
